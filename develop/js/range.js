da33ac7466eb6b8b13ee4687640f566c0c53f476
Christine Matthews <candreola@gmail.com>
2024-08-06 16:21:37 -0500

    make range functional and bpm show value

---
+++ develop/js/range.js
@@ -0,0 +1,30 @@
+const range = document.getElementById('customRange1');
+const rangeUp = document.getElementById('range-up');
+const rangeDown = document.getElementById('range-down');
+const bpm = document.getElementById('bpm-value');
+
+range.addEventListener('change', function () {
+    // console.log(range.value);
+    bpm.innerText = range.value;
+});
+
+range.addEventListener('input', function () {
+    bpm.innerText = range.value;
+   
+}) 
+
+
+
+
+rangeUp.addEventListener('click', function () {
+    // console.log('clicked')
+    range.value++
+    bpm.innerText = range.value
+
+});
+
+rangeDown.addEventListener('click', function () {
+    // console.log('clicked')
+    range.value--
+    bpm.innerText = range.value
+})
