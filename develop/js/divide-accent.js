03e424cb352e3395223f27f7145f8d7a808ffb1e
Christine Matthews <candreola@gmail.com>
2024-08-06 16:48:00 -0500

    make divide button change upon click

--- develop/js/divide-accent.js
+++ develop/js/divide-accent.js
@@ -1,3 +1,28 @@
 const divide = document.getElementById('divide');
 const accent = document.getElementById('accent');
+const divideText = document.getElementById('divide-text');
+const accentText = document.getElementById('accent-text');
 
+let count = 0
+function incrementCount() {
+    count++;
+}
+
+function decrementCount() {
+    if (count > 0) {
+        count--;
+    }
+}
+
+divide.addEventListener("click", () => {
+    incrementCount()
+    if (count === 1) {
+        divideText.innerText = "Divide by 2"
+    } else if (count === 2) {
+        divideText.innerText = "Divide by 4"
+    } else if (count > 2){
+        count = 0
+        divideText.innerText = "Divide by 0"
+
+    }
+})
