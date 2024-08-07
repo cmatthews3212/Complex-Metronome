11e17b51386915b9d0911c559a1b3f3457c0b8a5
Christine Matthews <candreola@gmail.com>
2024-08-07 14:13:26 -0500

    Add add group button to html and make sects clickable based on group buttons

--- develop/js/group-btns.js
+++ develop/js/group-btns.js
@@ -1,7 +1,26 @@
 const groupButtons = document.getElementsByClassName('button');
 const groupPlus = document.getElementsByClassName('')
 
-console.log(groupButtons)
+// console.log(groupButtons)
+
+let btnClicked1 = false;
+let btnClicked2 = false;
+let btnClicked3 = false;
+let btnClicked4 = false;
+let btnClicked5 = false;
+let btnClicked6 = false;
+let btnClicked7 = false;
+let btnClicked8 = false;
+let btnClicked9 = false;
+let btnClicked10 = false;
+let btnClicked11 = false;
+let btnClicked12 = false;
+let btnClicked13 = false;
+let btnClicked14 = false;
+let btnClicked15 = false;
+let btnClicked16 = false;
+let btnClicked = false;
+
 
 for(let i = 0; i < groupButtons.length; i++) {
     groupButtons[i].addEventListener('click', () => {
@@ -18,11 +37,59 @@ for(let i = 0; i < groupButtons.length; i++) {
                 btn.children[1].setAttribute('style', 'background-color: var(--red1); color: white;')
                 btn.children[2].setAttribute('style', 'color: var(--red1);')
                 btn.children[3].setAttribute('style', 'background-color: var(--red1); color: white;')
+                btnClicked1 = false;
+                btnClicked2 = false;
+                btnClicked3 = false;
+                btnClicked4 = false;
+                btnClicked5 = false;
+                btnClicked6 = false;
+                btnClicked7 = false;
+                btnClicked8 = false;
+                btnClicked9 = false;
+                btnClicked10 = false;
+                btnClicked11 = false;
+                btnClicked12 = false;
+                btnClicked13 = false;
+                btnClicked14 = false;
+                btnClicked15 = false;
+                btnClicked16 = false;
             }
         }
-    })
 
-    groupButtons[i].children[1].addEventListener('click', () => {
+        if (groupButtons[i] === groupButtons[0]) {
+            btnClicked1 = true;
+        } else if (groupButtons[i] === groupButtons[1]) {
+            btnClicked2 = true;
+        } else if (groupButtons[i] === groupButtons[2]) {
+            btnClicked3 = true;
+        } else if (groupButtons[i] === groupButtons[3]) {
+            btnClicked4 = true;
+        } else if (groupButtons[i] === groupButtons[4]) {
+            btnClicked5 = true;
+        } else if (groupButtons[i] === groupButtons[5]) {
+            btnClicked6 = true;
+        } else if (groupButtons[i] === groupButtons[6]) {
+            btnClicked7 = true;
+        } else if (groupButtons[i] === groupButtons[7]) {
+            btnClicked8 = true;
+        } else if (groupButtons[i] === groupButtons[8]) {
+            btnClicked9 = true;
+        } else if (groupButtons[i] === groupButtons[9]) {
+            btnClicked10 = true;
+        } else if (groupButtons[i] === groupButtons[10]) {
+            btnClicked11 = true;
+        } else if (groupButtons[i] === groupButtons[11]) {
+            btnClicked12 = true;
+        } else if (groupButtons[i] === groupButtons[12]) {
+            btnClicked13 = true;
+        } else if (groupButtons[i] === groupButtons[13]) {
+            btnClicked14 = true;
+        } else if (groupButtons[i] === groupButtons[14]) {
+            btnClicked15 = true;
+        } else if (groupButtons[i] === groupButtons[15]) {
+            btnClicked16 = true;
+        }
+
 
     })
 }
--- develop/js/subdivisions.js
+++ develop/js/subdivisions.js
@@ -1,7 +1,47 @@
 const subDiv = document.getElementsByClassName('sect');
+const groupNumber = document.getElementsByClassName('number');
 
-for(let i = 0; i < subDiv.length; i++) {
-    subDiv[i].addEventListener('click', () => {
-        subDiv[i].setAttribute('style', 'background-color: var(--red2);');
-    }) 
-}
+console.log(groupButtons)
+
+for(let i = 0; i < groupButtons.length; i++) {
+    for(let i = 0; i < subDiv.length; i++) {
+            subDiv[i].addEventListener('click', () => {
+                if (groupButtons[i].getAttribute('style', 'background-Color: var(--red1);') && btnClicked1 === true) {
+                    subDiv[i].setAttribute('style', 'background-color: var(--red3);');
+                } else if (groupButtons[i].getAttribute('style', 'background-Color: var(--red1);') && btnClicked2 === true) {
+                    subDiv[i].setAttribute('style', 'background-color: var(--red4);');
+                } else if (groupButtons[i].getAttribute('style', 'background-Color: var(--red1);') && btnClicked3 === true) {
+                    subDiv[i].setAttribute('style', 'background-color: var(--red3);');
+                } else if (groupButtons[i].getAttribute('style', 'background-Color: var(--red1);') && btnClicked4 === true) {
+                    subDiv[i].setAttribute('style', 'background-color: var(--red4);');
+                } else if (groupButtons[i].getAttribute('style', 'background-Color: var(--red1);') && btnClicked5 === true) {
+                    subDiv[i].setAttribute('style', 'background-color: var(--red3);');
+                } else if (groupButtons[i].getAttribute('style', 'background-Color: var(--red1);') && btnClicked6 === true) {
+                    subDiv[i].setAttribute('style', 'background-color: var(--red4);');
+                } else if (groupButtons[i].getAttribute('style', 'background-Color: var(--red1);') && btnClicked7 === true) {
+                    subDiv[i].setAttribute('style', 'background-color: var(--red3);');
+                } else if (groupButtons[i].getAttribute('style', 'background-Color: var(--red1);') && btnClicked8 === true) {
+                    subDiv[i].setAttribute('style', 'background-color: var(--red4);');
+                } else if (groupButtons[i].getAttribute('style', 'background-Color: var(--red1);') && btnClicked9 === true) {
+                    subDiv[i].setAttribute('style', 'background-color: var(--red3);');
+                } else if (groupButtons[i].getAttribute('style', 'background-Color: var(--red1);') && btnClicked10 === true) {
+                    subDiv[i].setAttribute('style', 'background-color: var(--red4);');
+                } else if (groupButtons[i].getAttribute('style', 'background-Color: var(--red1);') && btnClicked11 === true) {
+                    subDiv[i].setAttribute('style', 'background-color: var(--red3);');
+                } else if (groupButtons[i].getAttribute('style', 'background-Color: var(--red1);') && btnClicked12 === true) {
+                    subDiv[i].setAttribute('style', 'background-color: var(--red4);');
+                } else if (groupButtons[i].getAttribute('style', 'background-Color: var(--red1);') && btnClicked13 === true) {
+                    subDiv[i].setAttribute('style', 'background-color: var(--red3);');
+                } else if (groupButtons[i].getAttribute('style', 'background-Color: var(--red1);') && btnClicked14 === true) {
+                    subDiv[i].setAttribute('style', 'background-color: var(--red4);');
+                } else if (groupButtons[i].getAttribute('style', 'background-Color: var(--red1);') && btnClicked15 === true) {
+                    subDiv[i].setAttribute('style', 'background-color: var(--red3);');
+                } else if (groupButtons[i].getAttribute('style', 'background-Color: var(--red1);') && btnClicked16 === true) {
+                    subDiv[i].setAttribute('style', 'background-color: var(--red4);');
+                }
+            }) 
+
+        }
+    }
+        
+
